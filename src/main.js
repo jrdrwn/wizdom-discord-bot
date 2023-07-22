3ab826e63cb0c2ea83cda4b44e4a55b2dd333a20
rahmat <rahmatgamming1@gmail.com>
2023-07-22 16:50:03 +0800

    fix start command again

--- src/main.js
+++ src/main.js
@@ -4,6 +4,21 @@ const fs = require("fs");
 const path = require("path");
 const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
 const env = require("./config/env");
+const express = require("express");
+
+const app = express();
+
+app.post("/interactions", (req, res) => {
+  res.send("interactions ok");
+});
+
+app.get("/register_commands", (req, res) => {
+  res.send("register_commands ok");
+});
+
+app.get("/", async (req, res) => {
+  res.send("ok");
+});
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -62,4 +77,8 @@ client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.login(env.bot.token);
+app.listen(env.port, () => {
+  console.log(`Server is listening on port ${env.port}`);
+
+  client.login(env.bot.token);
+});
