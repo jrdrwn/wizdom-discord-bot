3737f2b9e7c0b7ddef311b673d00108dd2bdc2b0
Jordi Irawan <jordirwn@gmail.com>
2023-07-25 16:50:54 +0700

    wah

--- src/main.js
+++ src/main.js
@@ -71,18 +71,6 @@ client.once(Events.ClientReady, (c) => {
 app.post("/interactions", verifyKeyMiddleware(env.bot.clientId), (req, res) => {
   const message = req.body;
 
-  const signature = req.get("X-Signature-Ed25519");
-  const timestamp = req.get("X-Signature-Timestamp");
-  const isValidRequest = verifyKey(
-    req.rawBody,
-    signature,
-    timestamp,
-    env.bot.clientId
-  );
-  if (!isValidRequest) {
-    return res.status(401).end("Bad request signature");
-  }
-
   if (message.type === InteractionType.APPLICATION_COMMAND) {
     res.send({
       type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
